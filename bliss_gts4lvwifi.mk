42171f86e97629a68375f6449ca2cb54e7d42d49
srgrusso <srgrusso240@gmail.com>
2020-12-07 20:40:05 -0600

    gta4lvwifi: Add Camera2

--- bliss_gts4lvwifi.mk
+++ bliss_gts4lvwifi.mk
@@ -27,6 +27,10 @@ $(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
 # Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1440
 
+#Apps
+PRODUCT_PACKAGES += \
+    Camera2
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gts4lvwifi
 PRODUCT_NAME := bliss_gts4lvwifi
